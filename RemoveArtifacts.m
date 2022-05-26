3cd429c11c803a2712b9f004f4ae251a800c8e84
Sarah West <west0883@umn.edu>
2022-05-26 09:34:05 -0500

    Continue to update. Now actually works with masks.

--- RemoveArtifacts.m
+++ RemoveArtifacts.m
@@ -28,11 +28,25 @@ function [parameters] = RemoveArtifacts(parameters)
     
     % Put pixels into first dimension
     parameters.original_ICs = permute(parameters.original_ICs,[parameters.originalSourcesPixelsDim setxor(parameters.originalSourcesPixelsDim, 1:ndims(parameters.original_ICs))]);
+   
+    % Check to see if there was an existing artifacts removed structure. If
+    % not, establish all fields.
+    if ~isfield(parameters, 'sources_artifacts_removed')
+    
+       % Initialize list of indices to remove.
+       parameters.sources_artifacts_removed.indices_to_remove = cell(size(sources, parameters.sourcesDim),1);
+       
+       % Initialize empty list of sources to remove
+       parameters.sources_artifacts_removed.sources_removed = [];
+    end
+
+    % Initialize matrix of artifact-removed sources. (Do each time so you
+    % don't lose track of sourcei and original_sourcei).
+    parameters.sources_artifacts_removed.sources = sources; 
 
     % For each source to clean, 
     for sourcei = 1:size(sources, parameters.sourcesDim) 
         
-       
         % Get the source out from the variable dimensions
         S{parameters.sourcesDim} = sourcei;  
         source = sources(S{:});
@@ -41,56 +55,112 @@ function [parameters] = RemoveArtifacts(parameters)
         original_sourcei = parameters.sources.originalICNumber_domainsSplit(sourcei);
 
         % Fill the mask of the original source. 
-        original_source = abs(parameters.original_ICs(:, sourcei)); 
+        original_source = abs(parameters.original_ICs(:, original_sourcei)); 
         original_source = FillMasks(original_source, parameters.indices_of_mask, size(source,1), size(source,2));
 
-        
+        % Make find indices where the source is.
         indices = find(source > 0);
+
+        % Hold onto a version of the reference image.
         reference_image_old = reference_image;
+
+        % For the contex image, put the source in the reference image. 
         reference_image(indices) = source(indices);
         
+        % For image of brain behind the source, keep pixels of reference
+        % image that are only the source.
         indices = find(source == 0); 
         reference_image_old(indices) = 0; 
 
-        % Arrange figure;
-        figure; sgtitle([strjoin(parameters.values, ', ' ) ', source ' num2str(sourcei)]);
+        % ****Arrange figure****;
+        fig = figure; 
+
+        % Make full-screen
+        fig.WindowState = 'maximized';
+        
+        % Make figure title with all iterator values in it.
+        sgtitle([strjoin(parameters.values, ', ' ) ', source ' num2str(sourcei)]);
+        
+        % ** Context image **
         subplot(3,3,1);
         img = image(reference_image, 'CDataMapping', 'scaled');
-        set(img, 'AlphaData', ~isnan(source)); 
+        set(img, 'AlphaData', ~isnan(source)); % Make nans "transparent"
+        title('Context');
         
-        % Get the color range you'll use. 
+        % Get the color range you'll use for the contex image.
         top1 = max(max(source));
         top2 = max(max(reference_image)); 
         cmap = [parula(top1 * 10); gray((top2)*20)];
         colormap(cmap);
         axis square; xticks([]); yticks([]); 
         
+        % ** Draw a figure showing the brain underneath the source.**
         subplot(3, 3, 4);
         img3 = imagesc(reference_image_old);
-        set(img3, 'AlphaData', ~isnan(source)); 
+        set(img3, 'AlphaData', ~isnan(source)); % Make nans "transparent"
         colormap(gca,[0.5 0.5 0.5; gray(256)]);
         reference_image_old(find(reference_image_old == 0)) = NaN;
         caxis([min(min(reference_image_old)) max(max(reference_image_old))]);
-         xticks([]); yticks([]); axis square; axis square; 
+        title('brain beneath source');
+        xticks([]); yticks([]); axis square; axis square; 
         
+        % ** Draw a figure showing the original, un-thresholded source. ** 
         subplot(3,3,7); 
         img1 = imagesc(original_source);   
         set(img1, 'AlphaData', ~isnan(source)); 
         colormap(gca, parula(256)); caxis([0 10]); 
+        title('corresponding raw source');
         xticks([]); yticks([]); axis square;
         
+        % Draw the source, where you're going to draw masks.
         subplot(1,3, 2:3); 
         img2 = imagesc(source); caxis([0 10]);
         set(img2, 'AlphaData', ~isnan(source)); 
         cmap2 = [0.5 0.5 0.5; parula(256)];
         colormap(gca, cmap2); caxis([parameters.amplitude_threshold max(max(source))]); colorbar; 
         xticks([]); yticks([]);  axis square;
+       
+        % Grab axis handle for drawing on with ManualMasking
+        axis_for_drawing = gca; 
+
+        % *** Ask if the whole IC should be thrown out.***
+        user_answer1= inputdlg(['Do you want to throw out this entire source as an artifact? y=yes, n=no']); 
+       
+        %Convert the user's answer into a value
+        answer1=user_answer1{1};
+        
+        % If the user's answer is y (being strict/difficult with this so
+        % accidents are hard), mark it & move on to next source. Do nothing otherwise.
+        if strcmp('y', answer1)
+            
+            % Note source for removal. 
+            parameters.sources_artifacts_removed.indices_to_remove{sourcei} = 'all'; 
+            parameters.sources_artifacts_removed.sources_removed = [parameters.sources_artifacts_removed.sources_removed; sourcei]; 
+
+        else
+            % Run fine-tune removal of artifacts within ICs.
+            [~, indices_of_mask]=ManualMasking(source, [], axis_for_drawing);
+            
+            % Remove indices from source.  
+            parameters.sources_artifacts_removed.indices_to_remove{sourcei} = indices_of_mask;
+            source(indices_of_mask) = 0; 
+            parameters.sources_artifacts_removed.sources(S{:}) = source;
+        end
+   
     end
+   
+    % Remove sources that should be removed. 
+    S{parameters.sourcesDim} = parameters.sources_artifacts_removed.sources_removed;
+    parameters.sources_artifacts_removed.sources(S{:}) = []; 
+
+    % Upadate original (raw) source ID list. 
+    parameters.sources_artifacts_removed.originalICNumber = parameters.sources.originalICNumber_domainsSplit;
+    parameters.sources_artifacts_removed.originalICNumber(parameters.sources_artifacts_removed.sources_removed) = [];
 
     % Ask user if they want to work on next dataset 
     user_answer1= inputdlg(['Do you want to work on the next data set? 1=Y, 0=N']); 
 
-    %Convert the user's answer into a value
+    % Convert the user's answer into a value
     answer1=str2num(user_answer1{1});
     
     % If user didn't want to continue to next mouse, set continue flag to false
