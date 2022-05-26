a2970e75b872492273bc792669615df4b3dc5d6c
Sarah West <west0883@umn.edu>
2022-05-26 09:33:42 -0500

    Updated file directory formats.

--- plot_rawICs.m
+++ plot_rawICs.m
@@ -7,19 +7,14 @@ function []=plot_rawICs(parameters)
     
     % Return parameters to individual names.
     mice_all = parameters.mice_all;
-    dir_exper = parameters.dir_exper;
     num_sources = parameters.num_sources;
     yDim = parameters.yDim;
     xDim = parameters.xDim;
     masked_flag = parameters.masked_flag; 
     plot_sizes = parameters.plot_sizes;
-    masks_name = parameters.masks_name;
-
-    % Establish folder name you're working with. 
-    dir_out=[dir_exper 'ICs raw\'];
     
     % Tell user where data is being saved. 
-    disp(['data saved in ' dir_out]); 
+    disp(['data saved in ' parameters.dir_output_base{1}]); 
     
     % For each mouse
     for mousei=1:size(mice_all,2)  
@@ -27,20 +22,28 @@ function []=plot_rawICs(parameters)
         % Get the mouse name and display to user.
         mouse=mice_all(mousei).name;
 
-        % Load the raw sources. 
-        load([dir_out 'm' mouse '_' num2str(num_sources) 'sources.mat']); 
+        % Establish input folder name you're working with. 
+        filename = CreateFileStrings([parameters.dir_input_base parameters.input_filename], mouse, [], [], [], false);
+        input_variable = CreateFileStrings(parameters.input_variable, mouse, [], [], [], false);
+        
+        % Load the raw sources, convert variable to something generic.
+        load(filename, input_variable); 
+        eval(['sources = ' input_variable ';']);
         
         % Reshape and permute for plotting 
         
         % If the data was masked, load mask and reshape specially
         if masked_flag==1
+            
             % Find file name of masks
-            file_string_mask=CreateFileStrings(masks_name, mouse, [], [], false);
+            filename_mask=CreateFileStrings([parameters.dir_input_mask parameters.mask_filename], mouse, [], [], false);
+            mask_variable = CreateFileStrings(parameters.mask_variable, mouse, [], [], false);
+
+            % Load mask indices, convert variable to something generic. 
+            load(filename_mask, mask_variable); 
+            eval(['indices_of_mask = ' mask_variable ';']);
             
-            % Load mask indices 
-            load(file_string_mask, 'indices_of_mask'); 
-            
-            % flip sources for inputting into FillMasks
+            % Flip sources for inputting into FillMasks
             sources=sources';
             
             % Fill in masks with FillMasks.m function
@@ -51,11 +54,15 @@ function []=plot_rawICs(parameters)
             sources_permute=permute(reshape(sources, num_sources, yDim, xDim),[2 3 1]);
         end 
         
-        % Plot and save figures of sources. 
-        figure; for i=1:num_sources; subplot(plot_sizes(1), plot_sizes(2),i); imagesc(sources_permute(:,:,i)); end
-       
-        suptitle(['m' mouse ', ' num2str(num_sources) ' sources']); 
-        savefig([dir_out 'm' mouse '_' num2str(num_sources) 'sources.fig']); 
+        % Plot sources
+        figure; for i=1:num_sources; subplot(plot_sizes(1), plot_sizes(2),i); imagesc(abs(sources_permute(:,:,i))); end
+        sgtitle(['m' mouse ', ' num2str(num_sources) ' absolute value of sources']); 
+
+        % Get output names.
+        filename_output = CreateFileStrings([parameters.dir_output_base parameters.output_filename], mouse, [], [], [], false);
+
+        % Save
+        savefig(filename_output); 
     
     end 
 end 
