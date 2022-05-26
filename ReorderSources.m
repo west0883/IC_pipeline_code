f718b27accef110fc94678296ed6730919b5bea8
Sarah West <west0883@umn.edu>
2022-05-26 09:35:44 -0500

    Use new reordering format. Option to add sources together.

--- ReorderSources.m
+++ ReorderSources.m
@@ -4,17 +4,34 @@
 
 function [parameters] = ReorderSources(parameters)
 
+    % Tell user what you're doing.
+    MessageToUser('Reordering', parameters);
+
     % Make a holding matrix
-    parameters.sources_reordered = NaN(size(parameters.sources));
+    parameters.sources_reordered = NaN(parameters.yDim, parameters.xDim, parameters.num_new_sources);
 
-    % For each source in the manual assignments list
-    for sourcei = 1:size(parameters.assigned_region_order,1)
+    % For each (new) source/node in the manual assignments list
+    for sourcei = 1:size(parameters.assigned_region_order,1) 
         
-        % Get original place
-        original_place = parameters.assigned_region_order(sourcei);
+        % Get new place/source number
+        new_place = parameters.assigned_region_order(sourcei, 2); 
 
-        % Put source in new place.
-        parameters.sources_reordered(:,:, sourcei) = parameters.sources(:,:, original_place); 
+        % Get original place/source number
+        original_place = parameters.assigned_region_order(sourcei, 1);
+
+        % Put source in new place. 
         
+        % If user says so, do this by adding so multiple sources that
+        % belong to the same (new) source/node so they're both represented.
+        % Default is to over-write previous entries. 
+        if isfield(parameters, 'add_sources') && parameters.add_sources 
+           
+            holder = cat(3, parameters.sources_reordered(:,:, new_place), parameters.sources(:,:, original_place));
+            parameters.sources_reordered(:,:, new_place) =  sum(holder, 3, 'omitnan');
+        
+        % Otherwise, overwrite holder entry. 
+        else
+            parameters.sources_reordered(:,:, new_place) = parameters.sources(:,:, original_place); 
+        end 
     end
 end
