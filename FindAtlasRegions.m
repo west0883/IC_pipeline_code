25c274796e99d217d1953da4296c554919227ae3
Sarah West <west0883@umn.edu>
2022-05-26 09:35:05 -0500

    Include code for finding best-fitting atlas regions.

---
+++ FindAtlasRegions.m
@@ -0,0 +1,11 @@
+% FindAtlasRegions.m
+% Sarah West
+% 4/1/22
+
+% Uses center-of-mass and percent area to find the best atlas region for
+% each spatial source.
+
+function [parameters] = FindAtlasRegions(parameters)
+
+
+end 
--- pipeline_ICs.m
+++ pipeline_ICs.m
@@ -141,7 +141,7 @@ parameters.originalSourcesDim = 1;
 
 % Loop variables
 parameters.loop_list.iterators = {'mouse', {'loop_variables.mice_all(:).name'}, 'mouse_iterator';
-                                  'source', {'1:50'}, 'source_iterator'};
+                                  'source', {'7:50'}, 'source_iterator'};
 parameters.loop_variables.mice_all = parameters.mice_all;
 
 % Input values
@@ -179,10 +179,76 @@ parameters.loop_list.things_to_save.sources_artifacts_removed.filename = {'sourc
 parameters.loop_list.things_to_save.sources_artifacts_removed.variable= {'sources'};
 parameters.loop_list.things_to_save.sources_artifacts_removed.level = 'mouse';
 
-
 RunAnalysis({@RemoveArtifacts}, parameters);
 
+%% Return any deleted ICs you deleted by accident.
+% % Loop variables
+% parameters.loop_list.iterators = {'mouse', {'loop_variables.mice_all(:).name'}, 'mouse_iterator'};
+% parameters.loop_variables.mice_all = parameters.mice_all;
+% 
+% % Inputs 
+% % Sources
+% parameters.loop_list.things_to_load.sources_artifacts_removed.dir = {[parameters.dir_exper 'spatial segmentation\500 SVD components\artifacts_removed\'], 'mouse', '\'};
+% parameters.loop_list.things_to_load.sources_artifacts_removed.filename = {'sources.mat'};
+% parameters.loop_list.things_to_load.sources_artifacts_removed.variable= {'sources'};
+% parameters.loop_list.things_to_load.sources_artifacts_removed.level = 'mouse';
+% 
+% % Masks
+% parameters.loop_list.things_to_load.indices_of_mask.dir = {[parameters.dir_exper 'masks/']};
+% parameters.loop_list.things_to_load.indices_of_mask.filename= {'masks_m', 'mouse', '.mat'};
+% parameters.loop_list.things_to_load.indices_of_mask.variable= {'indices_of_mask'}; 
+% parameters.loop_list.things_to_load.indices_of_mask.level = 'mouse';
+
+
+% Outputs (editing the same file)
+% parameters.loop_list.things_to_load.sources_artifacts_removed.dir = {[parameters.dir_exper 'spatial segmentation\500 SVD components\artifacts_removed\'], 'mouse', '\'};
+% parameters.loop_list.things_to_load.sources_artifacts_removed.filename = {'sources.mat'};
+% parameters.loop_list.things_to_load.sources_artifacts_removed.variable= {'sources'};
+% parameters.loop_list.things_to_load.sources_artifacts_removed.level = 'mouse';
+% 
+% RunAnalysis({@RestoreSources}, parameters);
+
+
 %% Plot resulting cleaned ICs
+% Always clear loop list first. 
+% if isfield(parameters, 'loop_list')
+% parameters = rmfield(parameters,'loop_list');
+% end
+% 
+% % Dimension different sources are in.
+% parameters.sourcesDim = 3;
+% 
+% % Loop variables
+% parameters.loop_list.iterators = {'mouse', {'loop_variables.mice_all(:).name'}, 'mouse_iterator'};
+% parameters.loop_variables.mice_all = parameters.mice_all;
+% 
+% % Input variables
+% parameters.loop_list.things_to_load.sources_overlay.dir = {[parameters.dir_exper 'spatial segmentation\artifacts_removed\'], 'mouse', '\'};
+% parameters.loop_list.things_to_load.sources_overlay.filename = {'sources.mat'};
+% parameters.loop_list.things_to_load.sources_overlay.variable= {'sources.overlay'};
+% parameters.loop_list.things_to_load.sources_overlay.level = 'mouse';
+% 
+% % Output variables
+% parameters.loop_list.things_to_save.sources_overlay.dir = {[parameters.dir_exper 'spatial segmentation\artifacts_removed\'], 'mouse', '\'};
+% parameters.loop_list.things_to_save.sources_overlay.filename = {'sources_overlay.fig'};
+% parameters.loop_list.things_to_save.sources_overlay.level = 'mouse';
+% 
+% parameters.loop_list.things_to_save.sources_artifacts_removed.dir = {[parameters.dir_exper 'spatial segmentation\artifacts_removed\'], 'mouse', '\'};
+% parameters.loop_list.things_to_save.sources_artifacts_removed.filename = {'sources_overlay.fig'};
+% parameters.loop_list.things_to_save.sources_artifacts_removed.level = 'mouse';
+% 
+% % For now, assume everything was saved as structures from RemoveArtifacts.m
+% RunAnalysis({@PlotSourceOverlays}, parameters);
+
+%% Align atlases 
+% (Get from the LocaNMF preprocessing pipeline folder you made)
+
+%% Find best atlas locations. 
+% (from locomotion paper):
+% IC_groups_eachmouse.m
+% make_official_catalogue_permouse.m
+% find_coordinates_official_mousecatalogues.m
+
 % Always clear loop list first. 
 if isfield(parameters, 'loop_list')
 parameters = rmfield(parameters,'loop_list');
@@ -191,33 +257,61 @@ end
 % Dimension different sources are in.
 parameters.sourcesDim = 3;
 
-% Loop variables
+% Loop variables; iterate through mice, sources
 parameters.loop_list.iterators = {'mouse', {'loop_variables.mice_all(:).name'}, 'mouse_iterator'};
 parameters.loop_variables.mice_all = parameters.mice_all;
 
-% Input variables
-parameters.loop_list.things_to_load.sources_overlay.dir = {[parameters.dir_exper 'spatial segmentation\artifacts_removed\'], 'mouse', '\'};
-parameters.loop_list.things_to_load.sources_overlay.filename = {'sources.mat'};
-parameters.loop_list.things_to_load.sources_overlay.variable= {'sources.overlay'};
-parameters.loop_list.things_to_load.sources_overlay.level = 'mouse';
+% Load aligned atlas. 
+parameters.loop_list.things_to_load.atlas.dir = {[parameters.dir_exper 'spatial segmentation\aligned atlases\'], 'mouse'};
+parameters.loop_list.things_to_load.atlas.filename= {'atlas.mat'};
+parameters.loop_list.things_to_load.atlas.variable= {'atlas'}; 
+parameters.loop_list.things_to_load.atlas.level = 'mouse';
 
-% Output variables
-parameters.loop_list.things_to_save.sources_overlay.dir = {[parameters.dir_exper 'spatial segmentation\artifacts_removed\'], 'mouse', '\'};
-parameters.loop_list.things_to_save.sources_overlay.filename = {'sources_overlay.fig'};
-parameters.loop_list.things_to_save.sources_overlay.level = 'mouse';
+% Load corresponding atlas region names
+parameters.loop_list.things_to_load.region_names.dir = {[parameters.dir_exper 'spatial segmentation\aligned atlases\'], 'mouse'};
+parameters.loop_list.things_to_load.region_names.filename= {'atlas.mat'};
+parameters.loop_list.things_to_load.region_names.variable= {'regions'}; 
+parameters.loop_list.things_to_load.region_names.level = 'mouse';
 
-parameters.loop_list.things_to_save.sources_artifacts_removed.dir = {[parameters.dir_exper 'spatial segmentation\artifacts_removed\'], 'mouse', '\'};
-parameters.loop_list.things_to_save.sources_artifacts_removed.filename = {'sources_overlay.fig'};
-parameters.loop_list.things_to_save.sources_artifacts_removed.level = 'mouse';
+% Load mouse's mask, if necessary.
+parameters.loop_list.things_to_load.indices_of_mask.dir = {[parameters.dir_exper 'preprocessing\masks\']};
+parameters.loop_list.things_to_load.indices_of_mask.filename= {'masks_m', 'mouse', '.mat'};
+parameters.loop_list.things_to_load.indices_of_mask.variable= {'indices_of_mask'}; 
+parameters.loop_list.things_to_load.indices_of_mask.level = 'mouse';
 
-% For now, assume everything was saved as structures from RemoveArtifacts.m
-RunAnalysis({@PlotSourceOverlays}, parameters);
+% Load sources with artifacts removed
+parameters.loop_list.things_to_load.sources_artifacts_removed.dir = {[parameters.dir_exper 'spatial segmentation\500 SVD components\artifacts_removed\'], 'mouse', '\'};
+parameters.loop_list.things_to_load.sources_artifacts_removed.filename = {'sources.mat'};
+parameters.loop_list.things_to_load.sources_artifacts_removed.variable= {'sources.sources'};
+parameters.loop_list.things_to_load.sources_artifacts_removed.level = 'mouse';
 
-%% Group ICs into catalogues (interactive)
-% (from locomotion paper):
-% IC_groups_eachmouse.m
-% make_official_catalogue_permouse.m
-% find_coordinates_official_mousecatalogues.m
+% Also load sources WITHOUT artifacts removed (regularized only,to get the original center
+% of mass and area). 
+parameters.loop_list.things_to_load.sources.dir = {[parameters.dir_exper 'spatial segmentation\500 SVD components\regularized ICs_' num2str(parameters.area_threshold) 'pixels\'], 'mouse', '\'};
+parameters.loop_list.things_to_load.sources.filename= {['sources' num2str(parameters.num_sources) '.mat']};
+parameters.loop_list.things_to_load.sources.variable= {'sources'};
+parameters.loop_list.things_to_load.sources.level = 'mouse';
+
+% Save metrics 
+parameters.loop_list.things_to_save.metrics.dir = {[parameters.dir_exper 'spatial segmentation\500 SVD components\best region fit\'], 'mouse', '\'};
+parameters.loop_list.things_to_save.metrics.filename= {'best_region_fit_metrics.mat'};
+parameters.loop_list.things_to_save.metrics.variable= {'metrics'};
+parameters.loop_list.things_to_save.metrics.level = 'mouse';
+
+% Save figure of sources over atlas that meet a single best region
+parameters.loop_list.things_to_save.figure_best_fit.dir = {[parameters.dir_exper 'spatial segmentation\500 SVD components\best region fit\'], 'mouse', '\'};
+parameters.loop_list.things_to_save.figure_best_fit.filename= {'best_region_fit.fig'};
+parameters.loop_list.things_to_save.figure_best_fit.variable= {};
+parameters.loop_list.things_to_save.figure_best_fit.level = 'mouse';
+
+% Save figure of sources of atlas that have multiple potential best regions 
+parameters.loop_list.things_to_save.figure_multiple_best_fit.dir = {[parameters.dir_exper 'spatial segmentation\500 SVD components\best region fit\'], 'mouse', '\'};
+parameters.loop_list.things_to_save.figure_multiple_best_fit.filename= {'multiple_best_region_fit.fig'};
+parameters.loop_list.things_to_save.figure_multiple_best_fit.variable= {};
+parameters.loop_list.things_to_save.figure_multiple_best_fit.level = 'mouse';
+
+% Run code
+RunAnalysis({@FindAtlasRegions}, parameters);
 
 %% Apply IC masks to data & extract
 % (from locomotion paper):
